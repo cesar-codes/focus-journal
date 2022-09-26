57667b17058ca774cf3e692e5c97765936798b67
cesar-codes <cesar.avina@gmail.com>
2022-09-26 16:51:06 -0400

    clean commit before schema update

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const NoteSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
@@ -32,3 +32,40 @@ const PostSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Post", PostSchema);
+
+
+
+/* const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema); */
