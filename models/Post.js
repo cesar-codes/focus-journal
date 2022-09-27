de179d5cc90491373808bdeecd51e251587530cf
cesar-codes <cesar.avina@gmail.com>
2022-09-27 15:57:20 -0400

    add focus journal image

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,8 @@
+
+
 const mongoose = require("mongoose");
 
-const NoteSchema = new mongoose.Schema({
+const NewPostSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
@@ -31,41 +33,4 @@ const NoteSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Post", PostSchema);
-
-
-
-/* const mongoose = require("mongoose");
-
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    require: true,
-  },
-  cloudinaryId: {
-    type: String,
-    require: true,
-  },
-  caption: {
-    type: String,
-    required: true,
-  },
-  likes: {
-    type: Number,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model("Post", PostSchema); */
+module.exports = mongoose.model("Post", NewPostSchema); 
